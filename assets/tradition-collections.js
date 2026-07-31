f5db18c0f5e1fa72107cd1082bcf26236f0ad947
Subarna <sghorai814522@gmail.com>
2026-07-31 12:53:20 +0530

    new

--- assets/tradition-collections.js
+++ assets/tradition-collections.js
@@ -43,27 +43,7 @@ class TraditionCollections {
   }
 
   initCardInteractions() {
-    const cards = this.section.querySelectorAll('.tc-card');
-
-    cards.forEach((card) => {
-      card.addEventListener('mousemove', (e) => {
-        const rect = card.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-
-        const rotateX = (y - centerY) / 25;
-        const rotateY = (centerX - x) / 25;
-
-        card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-8px)`;
-      });
-
-      card.addEventListener('mouseleave', () => {
-        card.style.transform = '';
-      });
-    });
+    // 3D tilt effect removed to match Figma design
   }
 }
 
